ca45d97be64bc4760f1a9423696aec0bbd83dcbe
uuk0 <baulukas1301@googlemail.com>
2019-11-03 10:05:13 +0100

    added check function for index version

--- launcher/download.py
+++ launcher/download.py
@@ -5,14 +5,22 @@ import os
 
 DATA = None
 
+INDEX_VERSION = "1.0.0"
 
-def download_index():
-    response = urllib.request.urlopen("https://raw.githubusercontent.com/mcpython4-coding/Index/master/core.json")
+
+def download_index(url="https://raw.githubusercontent.com/mcpython4-coding/Index/master/core.json"):
+    response = urllib.request.urlopen(url)
     data = response.read()
     if os.path.exists(G.local+"/index/index.json"): os.remove(G.local+"/index/index.json")
     with open(G.local+"/index/index.json", mode="wb") as f: f.write(data)
     global DATA
     DATA = json.loads(data.decode("UTF-8"))
+    if "INDEX VERSION" not in DATA or DATA["INDEX VERSION"] != INDEX_VERSION:
+        if "OLD VERSION LINKS" in DATA and INDEX_VERSION in DATA["OLD VERSION LINKS"]:
+            url = DATA["OLD VERSION LINKS"][INDEX_VERSION]
+            download_index(url=url)
+        else:
+            raise ValueError("can't find readable index file for this version of the launcher")
 
 
 def download_by_name(name: str):
