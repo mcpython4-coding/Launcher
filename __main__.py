9bf413c7df0fc7f3228d3f7dc371533208994d07
uuk0 <baulukas1301@googlemail.com>
2020-07-15 15:54:40 +0200

    MDK is now able to select version to build on from launcher

--- __main__.py
+++ __main__.py
@@ -6,29 +6,8 @@ import shutil
 import time
 
 
-def create_or_skip(name: str):
-    if not os.path.exists(name): os.makedirs(name)
-
-
 if __name__ == "__main__":
-    create_or_skip(G.local+"/versions")
-    create_or_skip(G.local+"/home")
-    if "--delete-cache" in sys.argv:
-        for folder in os.listdir(G.local+"/home"):
-            if folder.startswith("cache"):
-                shutil.rmtree(G.local+"/home/"+folder)
-        sys.exit(-1)
-    if "--delete-versions" in sys.argv:
-        shutil.rmtree(G.local + "/versions")
-        sys.exit(-1)
-    if "--delete-all" in sys.argv:
-        a = input("you are going to loose ALL your data, are your sure to continue? (y/n) ").lower().strip()
-        if a == "y":
-            print("you have 5 sec to interrupt the process, than all data will get lost")
-            time.sleep(5)
-        shutil.rmtree(G.local+"/home")
-        shutil.rmtree(G.local+"/versions")
-        sys.exit(-1)
+    launcher.Launcher.setup()
     instance = launcher.Launcher.Launcher()
     if "--launch-profile" in sys.argv:
         instance.launch_profile(launcher.Launcher.Profile(G.local+"/home/{}".format(
--- launcher/Launcher.py
+++ launcher/Launcher.py
@@ -6,6 +6,32 @@ import sys
 import os
 import subprocess
 import shutil
+import time
+
+
+def create_or_skip(name: str):
+    if not os.path.exists(name): os.makedirs(name)
+
+
+def setup():
+    create_or_skip(G.local + "/versions")
+    create_or_skip(G.local + "/home")
+    if "--delete-cache" in sys.argv:
+        for folder in os.listdir(G.local + "/home"):
+            if folder.startswith("cache"):
+                shutil.rmtree(G.local + "/home/" + folder)
+        sys.exit(-1)
+    if "--delete-versions" in sys.argv:
+        shutil.rmtree(G.local + "/versions")
+        sys.exit(-1)
+    if "--delete-all" in sys.argv:
+        a = input("you are going to loose ALL your data, are your sure to continue? (y/n) ").lower().strip()
+        if a == "y":
+            print("you have 5 sec to interrupt the process, than all data will get lost")
+            time.sleep(5)
+        shutil.rmtree(G.local + "/home")
+        shutil.rmtree(G.local + "/versions")
+        sys.exit(-1)
 
 
 def create_or_stay(directory):
